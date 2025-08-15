12b15a5a531dc6d1a64c6d5af74cc697d8decb74
Awnish Kumar <awnish.cse@gmil.com>
2025-08-15 22:59:12 +0530

    first commit

---
+++ src/main/java/com/myspringbootproj/MyspringbootprojApplication.java
@@ -0,0 +1,13 @@
+package com.myspringbootproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbootprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myspringbootproj/MyspringbootprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringbootproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringbootprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
